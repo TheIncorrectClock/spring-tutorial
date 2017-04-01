4077a66286d337a9992d2655d3e898290563c2a9
Bartosz Wiśniewski <theincorrectclock@gmail.com>
2017-04-01 22:12:09 +0200

    Exercise 7 - Spring DI Java Config

---
+++ com.vogella.spring.first.di/src/main/java/com/vogella/spring/first/di/Application.java
@@ -0,0 +1,15 @@
+package com.vogella.spring.first.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.vogella.spring.first.di.config.Config;
+
+public class Application {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		ITodo todo = context.getBean(ITodo.class);
+		System.out.println(todo);
+		context.close();
+	}
+}
